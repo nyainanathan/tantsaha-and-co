44db5a1b919cf49b5442459ab443bdb121244ef8
Ny Aina Nathan A. <nyaina.nathan.1@gmail.com>
2026-04-20 10:05:17 +0300

    SPRING PROJECT INIT

---
+++ src/main/java/com/tantsaha/tantsaha/TantsahaApplication.java
@@ -0,0 +1,13 @@
+package com.tantsaha.tantsaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TantsahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TantsahaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tantsaha/tantsaha/TantsahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tantsaha.tantsaha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TantsahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
