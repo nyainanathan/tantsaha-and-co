659a72a55d04f9c8736bbace54c7161e11db58f8
Ny Aina Nathan A. <nyaina.nathan.1@gmail.com>
2026-04-22 18:09:35 +0300

    chore: DB tables updates

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,2 +1,23 @@
-alter table role_attribution
-add column attributed_at date default current_date;
+alter table cotisation
+drop column federation_share_pct;
+
+alter table cotisation
+drop column  cot_type;
+
+create type fees_frequency as enum('WEEKLY', 'MONTHLY', 'ANNUALLY', 'PUNCTUALLY');
+
+alter table cotisation
+add column frequency fees_frequency default 'WEEKLY';
+
+create type fees_status as enum('ACTIVE', 'INACTIVE');
+
+alter table cotisation
+add column status fees_status default 'ACTIVE';
+
+alter table cotisation
+drop column due_date;
+
+alter table cotisation
+add column eligibleFrom date default current_date;
+
+alter table cotisation rename to fee;
