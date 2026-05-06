59edca076a521bd851a8b5a2f3fff80c4e579dae
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-05-06 22:24:45 +0300

    refractor: lwk feel bad

---
+++ src/main/java/com/tantsaha/tantsaha/controller/dto/ActivityStatus.java
@@ -0,0 +1,6 @@
+package com.tantsaha.tantsaha.controller.dto;
+
+public enum ActivityStatus {
+    ACTIVE,
+    INACTIVE
+}
---
+++ src/main/java/com/tantsaha/tantsaha/entity/Transaction.java
@@ -0,0 +1,17 @@
+package edu.hei.school.agricultural.entity;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+    private String id;
+    private TransactionType type;
+    private Double amount;
+    private LocalDate creationDate;
+}
