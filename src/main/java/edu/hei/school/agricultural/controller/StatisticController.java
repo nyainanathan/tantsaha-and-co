272aeb45707c5df360c4377168a7f6c187d6b61c
Ania-su <antsaaa.randria@gmail.com>
2026-05-07 05:51:07 +0300

    feat: stat controller

---
+++ src/main/java/edu/hei/school/agricultural/controller/StatisticController.java
@@ -0,0 +1,50 @@
+package edu.hei.school.agricultural.controller;
+
+import edu.hei.school.agricultural.controller.mapper.StatisticDtoMapper;
+import edu.hei.school.agricultural.exception.BadRequestException;
+import edu.hei.school.agricultural.exception.NotFoundException;
+import edu.hei.school.agricultural.service.StatisticService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+
+public class StatisticController {
+
+    private final StatisticService statisticsService;
+    private final StatisticDtoMapper statisticsDtoMapper;
+
+    public StatisticController(StatisticService statisticsService, StatisticDtoMapper statisticsDtoMapper) {
+        this.statisticsService = statisticsService;
+        this.statisticsDtoMapper = statisticsDtoMapper;
+    }
+
+    @GetMapping("/collectivities/{id}/statistics")
+    public ResponseEntity<?> getLocalStatistics(@PathVariable String id,
+                                                @RequestParam LocalDate from,
+                                                @RequestParam LocalDate to) {
+        try {
+            return ResponseEntity.status(OK)
+                    .body(statisticsService.getLocalStatistics(id, from, to).stream()
+                            .map(statisticsDtoMapper::mapToDto)
+                            .toList());
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(BAD_REQUEST)
+                    .body(e.getMessage());
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+    }
+}
+
