4069a36856abcf83d296baaf9a133c20d2970acc
Ania-su <antsaaa.randria@gmail.com>
2026-05-07 05:50:28 +0300

    feat: stat dto and mapper

---
+++ src/main/java/edu/hei/school/agricultural/controller/dto/CollectivityLocalStatDto.java
@@ -0,0 +1,29 @@
+package edu.hei.school.agricultural.controller.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class CollectivityLocalStatDto {
+
+    private MemberDescriptionDto memberDescription;
+    private Double earnedAmount;
+    private Double unpaidAmount;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class MemberDescriptionDto {
+        private String id;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String occupation;
+    }
+}
+
---
+++ src/main/java/edu/hei/school/agricultural/controller/mapper/StatisticDtoMapper.java
@@ -0,0 +1,24 @@
+package edu.hei.school.agricultural.controller.mapper;
+
+import edu.hei.school.agricultural.controller.dto.CollectivityLocalStatDto;
+import edu.hei.school.agricultural.entity.CollectivityLocalStatistic;
+import edu.hei.school.agricultural.entity.Member;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatisticDtoMapper {
+    public CollectivityLocalStatDto mapToDto(CollectivityLocalStatistic entity) {
+        Member md = entity.getMemberDescription();
+        return CollectivityLocalStatDto.builder()
+                .memberDescription(CollectivityLocalStatDto.MemberDescriptionDto.builder()
+                        .id(md.getId())
+                        .firstName(md.getFirstName())
+                        .lastName(md.getLastName())
+                        .email(md.getEmail())
+                        .occupation(String.valueOf(md.getOccupation()))
+                        .build())
+                .earnedAmount(entity.getEarnedAmount())
+                .unpaidAmount(entity.getUnpaidAmount())
+                .build();
+    }
+}
